17dd6feb8277af85cbccc05c8169ed985b11ee52
dhurl4 <dhurl4@uic.edu>
2025-02-26 12:16:35 -0600

    Problem 1c

--- Project_1.v
+++ Project_1.v
@@ -112,7 +112,19 @@ Using the 1-bit full adder from either Part (a) or Part (b), design a 4-bit Ripp
 
 */
 
-// INSERT CODE FOR PART C HERE:
+module four_bit_RCA(A0, B0, A1, B1, A2, B2, A3, B3, Cin, S0, S1, S2, S3, Cout);
+    input A0, B0, A1, B1, A2, B2, A3, B3, Cin;
+    output S0, S1, S2, S3, Cout;
+
+    wire C1, C2, C3;
+
+    one_bit_full_adder FA0 (.A(A0), .B(B0), .Cin(Cin), .S(S0), .Cout(C1));
+    one_bit_full_adder FA1 (.A(A1), .B(B1), .Cin(C1), .S(S1), .Cout(C2));
+    one_bit_full_adder FA2 (.A(A2), .B(B2), .Cin(C2), .S(S2), .Cout(C3));
+    one_bit_full_adder FA3 (.A(A3), .B(B3), .Cin(C3), .S(S3), .Cout(Cout));
+
+
+endmodule
 
 // ************************************************************************************
 
